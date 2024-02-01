a04e516462d490f5735976eeccdb184242d33fc5
Nishadi <nish.shanika@gmail.com>
2024-02-01 06:22:08 +0100

    MealSharingHW-react2-week1

--- src/backend/api/meals.js
+++ src/backend/api/meals.js
@@ -2,13 +2,28 @@ const express = require("express");
 const router = express.Router();
 const knex = require("../database");
 
-router.get("/", async (request, response) => {
-  try {
+//router.get("/", async (request, response) => {
+  //try {
     // knex syntax for selecting things. Look up the documentation for knex for further info
-    const titles = await knex("meals").select("title");
-    response.json(titles);
-  } catch (error) {
-    throw error;
+   // const titles = await knex("meals").select("title");
+    //response.json(titles);
+  //} catch (error) {
+    //throw error;
+  //}
+//});
+
+router.get("/", async (request, response) => {
+  try{
+const mealWithDescription = await knex  ("meal")
+.leftJoin('Review', 'meal.id', 'Review.meal_id')
+.select("meal.title","meal.location","meal.price","Review.description")
+
+response.json(mealWithDescription);
+
+
+  }catch(error){
+    console.log("error", error)
+
   }
 });
 
--- src/client/App.js
+++ src/client/App.js
@@ -1,19 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import TestComponent from "./components/TestComponent/TestComponent";
+import MealList from "./components/MealList";
 
 function App() {
   return (
     <Router>
-      <Route exact path="/">
-        <p>test</p>
-      </Route>
-      <Route exact path="/lol">
-        <p>lol</p>
-      </Route>
-      <Route exact path="/test-component">
-        <TestComponent></TestComponent>
-      </Route>
+       <h1 style={{textAlign: "center"}}>Meal Sharing App</h1> 
+     <MealList/>
     </Router>
   );
 }
